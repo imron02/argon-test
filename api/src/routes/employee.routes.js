872b9ae780dc294927966c5a489947201fce9b1f
imron <imron.rosdiana@meteor.id>
2024-01-22 15:58:06 +0700

    refactor: add auth validation

--- api/src/routes/employee.routes.js
+++ api/src/routes/employee.routes.js
@@ -10,12 +10,13 @@ const {
   schemaCreate,
   schemaUpdate,
 } = require("../middlewares/employee.middleware");
+const { verifyToken } = require("../middlewares/auth.middleware");
 
 const router = express.Router();
 router.get("/employees", schemaCheckPagination, getEmployees);
 router.get("/employee/:id(\\d+)/", schemaCheckId, findOrDeleteEmployeeById);
 router.post("/employees", schemaCreate, postOrUpdateEmployees);
-router.put("/employee/:id", schemaUpdate, postOrUpdateEmployees);
+router.put("/employee/:id", [schemaUpdate, verifyToken], postOrUpdateEmployees);
 router.delete("/employee/:id", schemaCheckId, findOrDeleteEmployeeById);
 
 module.exports = router;
